c51f36cc195db147368d0f1510493ba032fc02d9
Maciej Kruszewski <podania.12@gmail.com>
2016-01-23 17:18:18 +0100

    Login sample with doctrine

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -9,36 +9,48 @@
 
 namespace Application\Controller;
 
-use Zend\Mvc\Controller\AbstractActionController;
+use Library\Db\Entity\UserTable;
+use Library\Helper\Services\Controller;
 use Zend\View\Model\ViewModel;
-use Application\Entity\UsersTable\User;
-use Doctrine\ORM\EntityManager;
 
-class IndexController extends AbstractActionController
+class IndexController extends Controller
 {
+
     public function indexAction()
     {
-        /**
-         * @var $entityManager EntityManager
-         */
-        $entityManager = $this
-            ->getServiceLocator()
-            ->get('Doctrine\ORM\EntityManager');
-
-//        $user = new User();
-//        $user->setId(202);
-//        $user->setName('Juljusz Słowacki');
-        /**
-         * @var $t User
-         */
-        $t = $entityManager->find('Application\Entity\UsersTable\User', 4);
-        echo $t->getName();
-//
-//        $entityManager->persist($user);
-//        $entityManager->flush();
-//
-//        echo $user->getId();
-
         return new ViewModel();
     }
+
+
+    public function loginAction()
+    {
+
+        /**
+         * @var $authService \Zend\Authentication\AuthenticationService
+         * @var $adapter \DoctrineModule\Authentication\Adapter\ObjectRepository
+         */
+
+        // If you used another name for the authentication service, change it here
+        $authService = $this->getServiceLocator()->get('Zend\Authentication\AuthenticationService');
+
+        $adapter = $authService->getAdapter();
+        $adapter->setIdentity('Adam');
+        $adapter->setCredential(md5('hehe'));
+        $authResult = $authService->authenticate();
+
+        if ($authResult->isValid()) {
+            echo 'logged<br>';
+            echo $authResult->getIdentity()->getLogin();
+//            return $this->redirect()->toRoute('home');
+        } else {
+            echo 'smfn is going wrong';
+        }
+
+        return new ViewModel(array(
+            'error' => 'Your authentication credentials are not valid',
+        ));
+
+    }
+
+
 }
